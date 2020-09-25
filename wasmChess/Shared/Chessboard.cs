9396bed0e1d73b8ce08748253f2b000e2b74035c
Mateusz Siewierski <siewier1@gmail.com>
2020-09-25 19:23:00 +0200

    3.0

---
+++ wasmChess/Shared/Chessboard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wasmChess.Shared
+{
+    public class Chessboard
+    {
+        private const int FilesNumber = 8;
+        private const int RanksNumber = 8;
+        private string whiteColor = "#e1e6e8";
+        private string blackColor = "#7f7fce";
+
+        private enum fileNames
+        {
+            A, B, C, D, E, F, G, H
+        }
+
+        public Square[,] squares;
+
+        public Chessboard()
+        {
+            squares = new Square[FilesNumber, RanksNumber];
+
+            for(int i = 0; i < FilesNumber; i++)
+            {
+                for(int j = 0; j < RanksNumber; j++)
+                {
+                    squares[i, j] = new Square(Char.Parse(Enum.GetName(typeof(fileNames), i)), j + 1, (i + j) % 2 == 0 ? blackColor : whiteColor);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ wasmChess/Shared/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wasmChess.Shared
+{
+    public class Square
+    {
+        private char file;
+        private int rank;
+        private string color;
+        public string FileName { get => "file-" + file; }
+        public string RankName { get => "rank-" + rank; }
+        public string Color { get => color; }
+        private bool isAvailable { get; set; }
+        public Square(char file, int rank, string color)
+        {
+            this.file = file;
+            this.rank = rank;
+            this.color = color;
+        }
+    }
+}
